8097bede57254244fab4f7bb1cbf42a670dea11b
Kangnam <zjnlove@foxmail.com>
2025-05-20 16:13:30 +0800

    ss

--- pages/_app.js
+++ pages/_app.js
@@ -34,52 +34,7 @@ function MyApp({ Component, pageProps }) {
 					{/* 规范链接 */}
 					<link rel="canonical" href={`${siteUrl}${pageProps.canonicalPath || ''}`} />
 					
-					{/* Open Graph 标签 */}
-					<meta property="og:site_name" content={`Worldle Unlimited`} />
-					<meta property="og:title" content={`Worldle - ${pageTitle}`} />
-					<meta property="og:description" content={pageDescription} />
-					<meta property="og:type" content="website" />
-					<meta property="og:url" content={`${siteUrl}${pageProps.canonicalPath || ''}`} />
-					<meta property="og:image" content={`${siteUrl}/${pageProps.ogImage || 'worldle-og-image.png'}`} />
-					<meta property="og:locale" content={locale || 'en'} />
 					
-					{/* Twitter 卡片 */}
-					<meta name="twitter:card" content="summary_large_image" />
-					<meta name="twitter:title" content={`Worldle - ${pageTitle}`} />
-					<meta name="twitter:description" content={pageDescription} />
-					<meta name="twitter:image" content={`${siteUrl}/${pageProps.ogImage || 'worldle-og-image.png'}`} />
-					
-					{/* 移动设备元标签 */}
-					<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
-					<meta name="mobile-web-app-capable" content="yes" />
-					<meta name="apple-mobile-web-app-capable" content="yes" />
-					
-					{/* 站点图标 */}
-					<link
-						rel="apple-touch-icon"
-						sizes="180x180"
-						href="/apple-touch-icon.png"
-					/>
-					<link
-						rel="icon"
-						type="image/png"
-						sizes="32x32"
-						href="/favicon-32x32.png"
-					/>
-					<link
-						rel="icon"
-						type="image/png"
-						sizes="16x16"
-						href="/favicon-16x16.png"
-					/>
-					<link rel="manifest" href="/site.webmanifest" />
-					<link
-						rel="mask-icon"
-						href="/safari-pinned-tab.svg"
-						color="#5bbad5"
-					/>
-					<meta name="msapplication-TileColor" content="#2d89ef" />
-					<meta name="theme-color" content="#ffffff" />
 					
 					{/* Google AdSense */}
 					<script
